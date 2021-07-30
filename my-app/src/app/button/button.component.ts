12db4ddf42cfc6ff5bf98ade179ba3027b5e348f
Sasha <ashatilovdev@gmail.com>
2021-07-30 19:13:37 +0300

    Добавил размеры и цвета

--- my-app/src/app/button/button.component.ts
+++ my-app/src/app/button/button.component.ts
@@ -6,19 +6,41 @@ import { btnColors, btnSize, isActive, isDisabled } from '../types/btnType';
   template: `
     <button class="btn"
     [ngClass]="[color]"
-    [style.fontSize]="'20px'"
-    [class.defaultSize]="size === 'defaultSize'">
+    [class.defaultSize]='size === "defaultSize"'
+    [class.small]='size === "small"'
+    [class.large]='size === "large"'
+    >
     {{btnText}}
     </button>
   `,
   styles: [`
   .btn{ border-radius: 10px; border: 1px solid #0099ff; color: #ffffff}
+  .btn:not(.active):not(.button-disabled):hover {
+    cursor: pointer;
+  }
   .default{background-color: #0099ff;}
-  .defaultSize{ width: 174px; height: 40px; }
+  .defaultSize{ 
+    padding: 10px 15px;
+    font-size: 16px;
+  }
+  .large {
+    padding: 15px 25px;
+    font-size: 16px;
+  }
+  .small {
+    padding: 5px 10px;
+    font-size: 12px;
+  }
+  .primary {
+    background-color: #3f51b5;
+  }
+  .accent {
+    background-color: #ff4081;
+  }
   `],
 })
 export class ButtonComponent implements OnInit {
-  btnText = 'Click me';
+  @Input() btnText = 'Click me';
   @Input() color: btnColors='default';
   @Input() size: btnSize='defaultSize';
   @Input() isActive: isActive=false
