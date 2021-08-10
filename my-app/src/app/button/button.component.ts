235a505ce7f009b46eb544c9f53cac38aea31444
Сержанова Виктория <victoriaserzhanova@gmail.com>
2021-08-10 11:58:11 +0000

    Update button.component.ts

--- my-app/src/app/button/button.component.ts
+++ my-app/src/app/button/button.component.ts
@@ -1,22 +1,25 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
 import { btnColors, btnSize, isActive, isDisabled } from '../types/btnType';
 
 @Component({
   selector: 'app-button',
   template: `
     <button class="btn"
-    [ngClass]="[color]"
+    [ngClass]="[innerColor]"
     [class.defaultSize]='size === "defaultSize"'
     [class.small]='size === "small"'
     [class.large]='size === "large"'
     [class.button-disabled]='isDisabled'
     [class.active]='innerActive'
     [attr.disabled]="isDisabled ? '' : null">
+
     {{btnText}}
     </button>
   `,
   styles: [`
-  .btn{ border-radius: 10px; border: 1px solid #0099ff; color: #ffffff}
+  .btn{ border-radius: 10px; border: 1px solid #0099ff; color: #ffffff;
+  opacity: 1;}
   .btn:not(.active):not(.button-disabled):hover {
     cursor: pointer;
   }
@@ -28,32 +31,41 @@ import { btnColors, btnSize, isActive, isDisabled } from '../types/btnType';
   .button-disabled { background-color: grey; border: 0px; }
   .active {
     cursor: default;
-    border: 2px solid rgb(185, 185, 185);
+    opacity: 0.6;
   }
   .defaultSize{padding: 10px 15px;font-size: 16px;}
   .large {padding: 15px 25px;font-size: 16px;}
   .small {padding: 5px 10px;font-size: 12px;}
+
   `],
 })
 export class ButtonComponent implements OnInit,OnChanges {
   @Input() btnText = 'Click me';
-  @Input() color: btnColors='default';
+  @Input() btnColor: btnColors='default';
   @Input() size: btnSize='defaultSize';
   @Input() isActive: isActive=false
   @Input() isDisabled: isDisabled=false;
-
+  innerColor:btnColors = 'default';
   innerActive: boolean = false;
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
+  ngOnChanges( changes: SimpleChanges): void {
+    const { btnColor } = changes;
     const { isActive } = changes;
-    if (isActive && isActive.currentValue) {
-      this.innerActive = isActive.currentValue;
+      if  ( btnColor && btnColor.currentValue ) {
+        this.innerColor = btnColor.currentValue;
+      }  
+
+      if (isActive && isActive.currentValue) {
+        this.innerActive = isActive.currentValue;
+      }
     }
-  }
+    
 
 }
